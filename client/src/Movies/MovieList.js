6a1dd45d551712c9794c73d96f19c6984c65b030
Pete James <petejames@petes-air.lan>
2019-12-07 11:54:28 -0800

    card links working

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom'; 
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -29,7 +30,9 @@ const MovieList = props => {
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
+  console.log('checking props', movie);
   return (
+    <Link to={`/movies/${movie.id}`}>
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
@@ -46,6 +49,7 @@ function MovieDetails({ movie }) {
         </div>
       ))}
     </div>
+    </Link>
   );
 }
 
